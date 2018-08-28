c06fcbe7c94961a4856a66f6de5a2553f4f498bb
Christopher Stull <chris.stull@gmail.com>
2018-08-28 21:10:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
   
   def add_item(thing,price,quantity=1)
-    @total = @total + price*quantity
+    @total = (@total + price*quantity)*(1-@discount/100)
   end
   
   def apply_discount
